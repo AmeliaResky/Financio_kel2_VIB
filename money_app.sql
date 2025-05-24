d6397464bb9b4690dfef3590ed2e4a8ea1e4c4f6
Amelia Resky <reskyamelia2406@gmail.com>
2025-05-24 19:52:55 +0700

    Update money_app.sql

--- money_app.sql
+++ money_app.sql
@@ -70,91 +70,58 @@ CREATE TABLE `user` (
 ALTER TABLE `batas_pengeluaran`
   ADD PRIMARY KEY (`id`);
 
---
--- Indeks untuk tabel `kategori_batas`
---
+-- Indeks untuk tabel `kategori_batas`--
 ALTER TABLE `kategori_batas`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nama_kategori` (`nama_kategori`);
 
---
--- Indeks untuk tabel `transaksi`
---
+-- Indeks untuk tabel `transaksi`--
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indeks untuk tabel `uang_keluar`
---
+-- Indeks untuk tabel `uang_keluar`--
 ALTER TABLE `uang_keluar`
   ADD PRIMARY KEY (`id`);
 
---
--- Indeks untuk tabel `uang_masuk`
---
+-- Indeks untuk tabel `uang_masuk`--
 ALTER TABLE `uang_masuk`
   ADD PRIMARY KEY (`id`);
 
---
--- Indeks untuk tabel `user`
---
+-- Indeks untuk tabel `user`--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `phone` (`phone`);
 
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
+-- AUTO_INCREMENT untuk tabel yang dibuang--
 
---
--- AUTO_INCREMENT untuk tabel `batas_pengeluaran`
---
+-- AUTO_INCREMENT untuk tabel `batas_pengeluaran`--
 ALTER TABLE `batas_pengeluaran`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT untuk tabel `kategori_batas`
---
+-- AUTO_INCREMENT untuk tabel `kategori_batas`--
 ALTER TABLE `kategori_batas`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT untuk tabel `transaksi`
---
+-- AUTO_INCREMENT untuk tabel `transaksi`--
 ALTER TABLE `transaksi`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT untuk tabel `uang_keluar`
---
+-- AUTO_INCREMENT untuk tabel `uang_keluar`--
 ALTER TABLE `uang_keluar`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
---
--- AUTO_INCREMENT untuk tabel `uang_masuk`
---
+-- AUTO_INCREMENT untuk tabel `uang_masuk`--
 ALTER TABLE `uang_masuk`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
---
--- AUTO_INCREMENT untuk tabel `user`
---
+-- AUTO_INCREMENT untuk tabel `user`--
 ALTER TABLE `user`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `transaksi`
---
+-- Ketidakleluasaan untuk tabel `transaksi`--
 ALTER TABLE `transaksi`
   ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
